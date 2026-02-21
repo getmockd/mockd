4d51ae8c13797f3bd33d2d2793d624ba2df9e6a6
Zach Snell <zach20snell10@gmail.com>
2026-02-20 23:24:26 -0600

    chore: map mockd version dynamically with debug.ReadBuildInfo
    Update `mockd version` to extract git VCS revisions and `-dirty` modified states automatically without requiring hardcoded flags at compile time. Fixes unused import. Updates README.md and documentation references to explicitly use the new scoped protocol add commands like `mockd http add`.

--- pkg/cli/version.go
+++ pkg/cli/version.go
@@ -1,20 +1,13 @@
 package cli
 
 import (
-	"flag"
 	"fmt"
-	"os"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/getmockd/mockd/pkg/cli/internal/output"
+	"github.com/spf13/cobra"
 )
-
-// BuildInfo contains build-time information
-type BuildInfo struct {
-	Version   string
-	Commit    string
-	BuildDate string
-}
 
 // VersionOutput represents JSON output format
 type VersionOutput struct {
@@ -26,56 +19,59 @@ type VersionOutput struct {
 	Arch    string `json:"arch"`
 }
 
-// RunVersion handles the version command
-func RunVersion(info BuildInfo, args []string) error {
-	fs := flag.NewFlagSet("version", flag.ContinueOnError)
-	jsonOutput := fs.Bool("json", false, "Output version in JSON format")
-	fs.Usage = func() {
-		fmt.Fprint(os.Stderr, `Usage: mockd version [flags]
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show mockd version information",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		version := Version
+		commit := Commit
+		date := BuildDate
 
-Show mockd version information.
-
-Flags:
-  --json    Output in JSON format
-
-Examples:
-  mockd version
-  mockd version --json
-`)
-	}
-
-	if err := fs.Parse(args); err != nil {
-		if err == flag.ErrHelp {
-			return nil
+		if info, ok := debug.ReadBuildInfo(); ok {
+			if version == "dev" {
+				version = info.Main.Version
+			}
+			for _, setting := range info.Settings {
+				switch setting.Key {
+				case "vcs.revision":
+					if commit == "none" {
+						commit = setting.Value
+					}
+				case "vcs.time":
+					if date == "unknown" {
+						date = setting.Value
+					}
+				case "vcs.modified":
+					if setting.Value == "true" {
+						commit += "-dirty"
+					}
+				}
+			}
 		}
-		return err
-	}
 
-	if fs.NArg() > 0 {
-		return fmt.Errorf("unexpected arguments: %v", fs.Args())
-	}
+		out := VersionOutput{
+			Version: version,
+			Commit:  commit,
+			Date:    date,
+			Go:      runtime.Version(),
+			OS:      runtime.GOOS,
+			Arch:    runtime.GOARCH,
+		}
 
-	out := VersionOutput{
-		Version: info.Version,
-		Commit:  info.Commit,
-		Date:    info.BuildDate,
-		Go:      runtime.Version(),
-		OS:      runtime.GOOS,
-		Arch:    runtime.GOARCH,
-	}
+		if jsonOutput {
+			return output.JSON(out)
+		}
 
-	if *jsonOutput {
-		return output.JSON(out)
-	}
+		v := out.Version
+		if len(v) > 0 && v[0] != 'v' && v != "dev" && v != "(devel)" {
+			v = "v" + v
+		}
+		fmt.Printf("mockd %s (%s, %s)\n", v, out.Commit, out.Date)
+		fmt.Printf("%s %s/%s\n", out.Go, out.OS, out.Arch)
+		return nil
+	},
+}
 
-	// Format: mockd v0.1.0 (abc1234, 2025-01-06)
-	//         go1.21.0 linux/amd64
-	// Note: Version may already include 'v' prefix from git tags
-	version := out.Version
-	if len(version) > 0 && version[0] != 'v' {
-		version = "v" + version
-	}
-	fmt.Printf("mockd %s (%s, %s)\n", version, out.Commit, out.Date)
-	fmt.Printf("%s %s/%s\n", out.Go, out.OS, out.Arch)
-	return nil
+func init() {
+	rootCmd.AddCommand(versionCmd)
 }
